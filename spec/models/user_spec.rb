5dd6c8bcc55cd923f03897e0ddf8ddf40f39a47f
Michael Lubensky <lubenskyy@gmail.com>
2014-04-12 11:27:43 -0500

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,7 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+end
 
   describe "return value of authenticate method" do
     before { @user.save }
@@ -29,6 +30,25 @@ describe User do
 
     describe "with valid password" do
       it { should eq found_user.authenticate(@user.password) }
+    end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
     end
 
     describe "with invalid password" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,35 +1,40 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe "User pages" do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 
-  describe "Help page" do
-    before { visit help_path }
+  describe "signup" do
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
-  end
+    before { visit signup_path }
 
-  describe "About page" do
-    before { visit about_path }
+    let(:submit) { "Create my account" }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
 
-  describe "Contact page" do
-    before { visit contact_path }
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
