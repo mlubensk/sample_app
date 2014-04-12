cb3c5ca0b50afc1342cd288851f7db0c3fba1f66
Michael Lubensky <lubenskyy@gmail.com>
2014-04-11 22:28:43 -0500

    password check

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+ describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+end
